2967f0909dc7b1c42e457fb887c7ca9f685656cb
robert <soarerobert17@gmail.com>
2022-04-14 10:36:40 +0300

    Add install steps

--- chrome-extension/content/contentScript.js
+++ chrome-extension/content/contentScript.js
@@ -1,23 +1,23 @@
-/**
- * Inject the `queryEngine` in the page.
- * @type {HTMLScriptElement}
- */
-
 window.addEventListener('load', (event) => {
-    const settingsBar = document.querySelector("#editor div.edit-post-header__settings")
-    if( settingsBar ) {
-        var s = document.createElement('script');
-        s.defer = true;
-        s.type = "module"
-        s.src = "https://cdn.jsdelivr.net/gh/Codeinwp/otter-query-engine@master/dist/index.js";
-        (document.head||document.documentElement).appendChild(s);
-        console.log(s)
-        s.onload = function() {
-           //  s.parentNode.removeChild(s);
-           const btn = document.createElement('span')
-           btn.innerText = "Query Engine Active"
-           settingsBar.insertBefore(btn, settingsBar.firstChild)
-           
-        };
-    }
+    let count = 0;
+    const interv = setInterval( () => {
+        count ++;
+        const settingsBar = document.querySelector("#editor div.edit-post-header__settings")
+        if( settingsBar ) {
+            var s = document.createElement('script');
+            s.defer = true;
+            s.type = "module";
+            s.src = "https://cdn.jsdelivr.net/gh/Codeinwp/otter-query-engine@master/dist/index.js";
+            (document.head||document.documentElement).appendChild(s);
+            console.log(s)
+            s.onload = function() {
+            //  s.parentNode.removeChild(s);
+            
+            };
+            clearInterval(interv);
+        }
+        if(count > 6) {
+            clearInterval(interv);
+        }
+    }, 500);
 });
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 
 ## Getting started
 
-Use together with [Query Engine](https://github.com/Soare-Robert-Daniel/otter-query-engine)
+Use together with [Query Engine](https://github.com/Codeinwp/otter-query-engine)
 
 You can use the chrome browser extension to add the script in every page. You can add the folder `chrome-extension` via [Developer Mode](https://developer.chrome.com/docs/extensions/mv3/faq/#:~:text=You%20can%20start%20by%20turning,a%20packaged%20extension,%20and%20more.)
 
@@ -11,3 +11,13 @@ You can use the chrome browser extension to add the script in every page. You ca
 
 Every folder represent a source/database from which we can get the exported blocks. Every folder that is used a source/database must have `index.json` in which we list the available files and their features. See `blocks/index.json` as example.
 
+## Chrome Extension Install
+
+#### Steps
+
+1. Enable developer mode in **Tools > Extensions**. The toggle is in the top-right corner.  
+![The toggle is in the top-right corner](images/toggle-location.png)
+2. Load the extensions via **Load unpacked**  
+![Load unpacked](images/unpacked-button.png)
+3. Select the **chrome-extension** folder  
+![Select the folder](images/select-folder.png)
